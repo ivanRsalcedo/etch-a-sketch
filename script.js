bd899a72184dd46d8d78835a20b61e2a66a189bf
Ivan Roderick Salcedo <nj.ivan.salcedo@gmail.com>
2025-07-08 18:41:37 -0400

    Add custom grid dimensions feature via user prompt

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
+let gridSize = 480;
+let gridDimensions = 16;
+
 const grid = document.querySelector('#grid');
 const btnSize = document.querySelector('#btn-size');
 
 btnSize.addEventListener('click', () => {
-    
+    gridDimensions = prompt('Number of squares per side:', 16);
+    clearGrid();
+    generateGrid();
 });
 
-let gridSize = 480;
-let gridDimensions = 16;
+generateGrid();
 
-for (let i = 0; i < gridDimensions * gridDimensions; i++) {
-    createCell();
+function clearGrid(){
+    grid.innerHTML = '';
 }
-    
-function createCell () {
-    const cell = document.createElement('div');
-    cell.style.width = `${calcCellSize()}px`;
-    cell.style.height = `${calcCellSize()}px`;
-    cell.addEventListener('mouseenter', () => {
-        cell.style.backgroundColor = '#666';
-    });
-    cell.classList.add('cell');
-    grid.append(cell);
+
+function generateGrid() {
+    for (let i = 0; i < gridDimensions * gridDimensions; i++) {
+        const cell = document.createElement('div');
+        cell.style.width = `${calcCellSize()}px`;
+        cell.style.height = `${calcCellSize()}px`;
+        cell.addEventListener('mouseenter', () => {
+            cell.style.backgroundColor = '#666';
+        });
+        cell.classList.add('cell');
+        grid.append(cell);
+    }
 }
 
 function calcCellSize() {
