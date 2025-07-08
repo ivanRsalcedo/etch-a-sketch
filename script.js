988b9f78118218f5af55f3393be4468b1adc379b
Ivan Roderick Salcedo <nj.ivan.salcedo@gmail.com>
2025-07-08 17:59:49 -0400

    Create grid generator and starting layout design

---
+++ script.js
@@ -0,0 +1,19 @@
+const grid = document.querySelector('#grid');
+let gridSize = 480;
+let gridDimensions = 16;
+
+for (let i = 0; i < gridDimensions * gridDimensions; i++) {
+    createCell();
+}
+    
+function createCell () {
+    const cell = document.createElement('div');
+    cell.style.width = `${calcCellSize()}px`;
+    cell.style.height = `${calcCellSize()}px`;
+    cell.classList.add('cell');
+    grid.append(cell);
+}
+
+function calcCellSize() {
+    return gridSize / gridDimensions;
+}
